39f91700718cc92254735ffae6423d484070ff2b
yuvarajnaga@users.noreply.github.com <yuvaraj.blisslogix@gmail.com>
2018-06-30 19:29:51 +0530

    table list

--- src/app/document-manager/document-manager/document-manager.component.ts
+++ src/app/document-manager/document-manager/document-manager.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
-import { MatPaginator } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 import { ResponsiveTableHelpers } from '../helpers.data';
 import { Router } from '@angular/router';
 import { GeneralServiceService } from '../../service/general-service.service';
@@ -10,54 +10,67 @@ import { GeneralServiceService } from '../../service/general-service.service';
   styleUrls: ['./document-manager.component.scss']
 })
 export class DocumentManagerComponent implements OnInit {
-  displayedColumns = ['userId', 'userName', 'progress', 'color'];
-	rows: Array<any> = [];
-    showResponsiveTableCode;
+  displayedColumns = ['basename', 'basetable', 'documenttype', 'databasename', 'action'];
+  rows: Array<any> = [];
+  dataSource;
+  showResponsiveTableCode;
+  @ViewChild(MatPaginator) paginator1: MatPaginator;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  pageLength = 0;
+  pageSize = 3;
+  docList;// = ResponsiveTableHelpers;
+  @Input() status;
+  @Input() actionStatus;
+  @Output() edit = new EventEmitter();
+  @Output() delete = new EventEmitter();
+  @Output() view = new EventEmitter();
+  @Output() page = new EventEmitter();
+  @Output() sort = new EventEmitter();
+  @Output() dup = new EventEmitter();
+  requestData = {
+    "dbModel": "sqlModel",
+    "database": "mssql"
+  }
+  constructor(private router: Router, public generalService: GeneralServiceService) {
+  }
 
-	@ViewChild(MatPaginator) paginator1: MatPaginator;
-    pageLength = 0;
-    pageSize = 3;
-    docList;// = ResponsiveTableHelpers;
-    @Input() status;
-    @Input() actionStatus;
-    @Output() edit = new EventEmitter();
-    @Output() delete = new EventEmitter();
-    @Output() view = new EventEmitter();
-    @Output() page = new EventEmitter();
-    @Output() sort = new EventEmitter();
-    @Output() dup = new EventEmitter();
-    requestData = {
-      "dbModel": "sqlModel",
-      "database": "mssql"
+  ngOnInit() {
+    this.generalService.getMetaDataList('listDocuments', this.requestData).subscribe
+      (repsonse => {
+        debugger;
+        this.docList = repsonse['metaDataRelatedTables'].metaDataResult;;
+        this.getRows();
+      });
+  }
+  next(event) {
+    debugger;
+    this.rows = [];
+    for (var i = 1 * event.pageIndex * event.pageSize; i < event.pageSize + event.pageIndex * event.pageSize; i++) {
+      this.rows = [...this.rows, this.docList[i]];
+      this.dataSource = new MatTableDataSource(this.docList);
+      this.dataSource.paginator = this.paginator;
+      console.log(this.rows);
     }
-    constructor(private router: Router, public generalService: GeneralServiceService) {
-   	}
+  }
+  getRows() {
+    for (var i = 0; i < this.pageSize; i++) {
+      this.rows = [...this.rows, this.docList[i]];
+      this.dataSource = new MatTableDataSource(this.docList);
+      this.dataSource.paginator = this.paginator;
 
-    ngOnInit() {
-      this.generalService.getMetaDataList('listDocuments', this.requestData).subscribe
-        (repsonse => {
-          debugger;
-          this.docList = repsonse['metaDataRelatedTables'].metaDataResult;
-        });
-        //this.getRows();
     }
-  	next(event) {
-        this.rows = [];
-    	for (var i= 1 * event.pageIndex * event.pageSize; i< event.pageSize+event.pageIndex*event.pageSize ;i++) {
-        this.rows = [...this.rows, this.docList.rows[i]];
-        }
-    }
-    getRows() {
-        for (var i=0;i<this.pageSize;i++) {
-          this.rows = [...this.rows, this.docList.rows[i]];
-        }
-        this.pageLength = this.docList.rows.length;
-    }
-    sortData(val){
-    }
+    this.pageLength = this.docList.length;
+  }
+  sortData(val) {
+  }
 
-    addNewDocument() {
-        this.router.navigate(['/document-manager/addnew']);
-    }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  addNewDocument() {
+    this.router.navigate(['/document-manager/addnew']);
+  }
 }
